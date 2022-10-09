3657ba10cb977e314eb54290e489dd3eeb388441
WolfPackStatMathieu <mthomassin747@gmail.com>
2022-10-09 10:47:22 +0200

    ajout point.py

---
+++ App1-Geometry-game/point.py
@@ -0,0 +1,9 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rectangle:
+    def __init__(self, pointA, pointB):
+        self.pointA = pointA
+        self.pointB = pointB
